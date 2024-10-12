32aba18d696ba9e59079ee74d201f12588cbd6f3
Dee-McG <mcgirr.daisy@gmail.com>
2024-10-12 20:47:08 +0100

    Switch summary to an update form, initial form if a row exists

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, UpdateView
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -17,19 +17,27 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['summary_form'] = SummaryForm()
+        try:
+            context['summary_form'] = SummaryForm(instance=Summary.objects.get(user=self.request.user))
+            context['summary'] = Summary.objects.get(user=self.request.user).summary
+        except:
+            context['summary_form'] = SummaryForm()
         return context
 
 
-class CreateSummary(LoginRequiredMixin, CreateView):  
+class UpdateSummary(LoginRequiredMixin, UpdateView):  
     model = Summary
     form_class = SummaryForm
 
     def form_valid(self, form):
+        print('Valid form')
+        form.instance.summary = form.cleaned_data['summary']
+        print(form.instance.summary)
         form.instance.user = self.request.user
         form.save()
         return HttpResponse('<p class="success">Form submitted successfully!</p>')  
 
     def form_invalid(self, form):
-        return HttpResponse('<p class="error">Please provide a summary.</p>')
+        print('Invalid form')
+        return HttpResponse('<p class="error">Please provide a summary. Max 500 chars</p>')
   
