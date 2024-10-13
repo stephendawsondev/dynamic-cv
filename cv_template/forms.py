1be4c37dc51ed138a1966044cf94956143b6900c
Dee-McG <mcgirr.daisy@gmail.com>
2024-10-13 20:24:08 +0100

    Add new create cv form and toasts

--- cv_template/forms.py
+++ cv_template/forms.py
@@ -2,9 +2,30 @@ from django import forms
 from django_ckeditor_5.widgets import CKEditor5Widget
 from .models import CVTemplate
 
+from profiles.models import Skill, WorkExperience, Education, ContactInformation
+
+
+class CustomMMCF(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, instance):
+        return '%s' % instance.__str__()
+
 
 class CVTemplateForm(forms.ModelForm):
     """ Form to create a CV """
+    def __init__(self, *args, **kwargs):
+        """ Grants access to the request object so that only members of the current user
+        are given as options"""
+
+        self.request = kwargs.pop('request')
+        super(CVTemplateForm, self).__init__(*args, **kwargs)
+        self.fields['contact_information'].queryset = ContactInformation.objects.filter(user=self.request.user)
+        self.fields['skills'].queryset = Skill.objects.filter(
+            user=self.request.user)
+        self.fields['work_experience'].queryset = WorkExperience.objects.filter(
+            user=self.request.user)
+        self.fields['education'].queryset = Education.objects.filter(
+            user=self.request.user)
+
     class Meta:
         model = CVTemplate
         fields = [
@@ -18,7 +39,20 @@ class CVTemplateForm(forms.ModelForm):
         ]
     
         widgets = {
-                "summary": CKEditor5Widget(
-                    attrs={"class": "django_ckeditor_5"}, config_name="default"
+                'summary': CKEditor5Widget(
+                    attrs={'class': "django_ckeditor_5"}, config_name="default"
                 )
             }
+        
+    skills = CustomMMCF(
+        queryset=None,
+        widget=forms.CheckboxSelectMultiple
+    )
+    work_experience = CustomMMCF(
+        queryset=None,
+        widget=forms.CheckboxSelectMultiple
+    )
+    education = CustomMMCF(
+        queryset=None,
+        widget=forms.CheckboxSelectMultiple
+    )
--- cv_template/views.py
+++ cv_template/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import (UserPassesTestMixin, LoginRequiredMixin)
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 from .models import CVTemplate
 from .forms import CVTemplateForm
+
+from profiles.models import Summary
 
 
 class CvList(ListView):
@@ -11,9 +13,29 @@ class CvList(ListView):
     template_name = 'cv_template/cv_list.html'
 
 
-class CreateCV(CreateView):
+class CreateCV(LoginRequiredMixin, CreateView):
     model = CVTemplate
     form_class = CVTemplateForm
     template_name = 'cv_template/cv_template.html'
     success_message = "CV created successfully"
-    success_url = 'cv/cv_list/'
+    success_url = '/'
+
+    def get_form_kwargs(self):
+        """ Passes the request object to the form class.
+         This is necessary to only display members that belong to a given user"""
+
+        kwargs = super(CreateCV, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        use_default_summary = form.cleaned_data.get('use_default_summary')
+        if use_default_summary:
+            try:
+                summary = Summary.objects.get(user=self.request.user).summary
+            except Summary.DoesNotExist:
+                summary = ""
+            form.instance.summary = summary
+        messages.success(self.request, "CV created successfully")
+        return super().form_valid(form)
