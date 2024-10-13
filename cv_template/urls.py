a2345f4b265a428d5176e3b6a50c43f184ad42e2
Dee-McG <mcgirr.daisy@gmail.com>
2024-10-13 20:35:26 +0100

    Formatting

--- cv_template/urls.py
+++ cv_template/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cv-list/', views.CvList.as_view(), name='cv_list'),
-    path('cv-template/', views.CreateCV.as_view(), name='cv_template'),
+    path("cv-list/", views.CvList.as_view(), name="cv_list"),
+    path("cv-template/", views.CreateCV.as_view(), name="cv_template"),
 ]
--- cv_template/views.py
+++ cv_template/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, CreateView
-from django.contrib.auth.mixins import (UserPassesTestMixin, LoginRequiredMixin)
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib import messages
 
 from .models import CVTemplate
@@ -10,27 +10,26 @@ from profiles.models import Summary
 
 class CvList(ListView):
     model = CVTemplate
-    template_name = 'cv_template/cv_list.html'
+    template_name = "cv_template/cv_list.html"
 
 
 class CreateCV(LoginRequiredMixin, CreateView):
     model = CVTemplate
     form_class = CVTemplateForm
-    template_name = 'cv_template/cv_template.html'
-    success_message = "CV created successfully"
-    success_url = '/'
+    template_name = "cv_template/cv_template.html"
+    success_url = "/"
 
     def get_form_kwargs(self):
-        """ Passes the request object to the form class.
-         This is necessary to only display members that belong to a given user"""
+        """Passes the request object to the form class.
+        This is necessary to only display members that belong to a given user"""
 
         kwargs = super(CreateCV, self).get_form_kwargs()
-        kwargs['request'] = self.request
+        kwargs["request"] = self.request
         return kwargs
-    
+
     def form_valid(self, form):
         form.instance.user = self.request.user
-        use_default_summary = form.cleaned_data.get('use_default_summary')
+        use_default_summary = form.cleaned_data.get("use_default_summary")
         if use_default_summary:
             try:
                 summary = Summary.objects.get(user=self.request.user).summary
