24425ada39000dc65503979592295bc6cd8b718e
Dee-McG <mcgirr.daisy@gmail.com>
2024-10-14 10:55:59 +0100

    Finish styling and responsiveness of create cv form

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,15 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProfileView.as_view(), name="profile"),
-    path('contact-information/', views.CreateUpdateContactInformation.as_view(),
-         name='contact-information'),
-    path('update-summary/', views.UpdateSummary.as_view(), name='update-summary'),
-    path('add-skill/<slug:skill>/', views.AddSkill.as_view(), name='add_skill'),
-    path('remove-skill/<slug:skill>/', views.RemoveSkill.as_view(),
-         name="remove_skill"),
-    path('add-responsibility/', views.AddResponsibility.as_view(),
-         name="add_responsibility"),
-    path('add-work_experience/', views.AddWorkExperience.as_view(),
-         name="add_work_experience"),
+    path("", views.ProfileView.as_view(), name="profile"),
+    path(
+        "contact-information/",
+        views.CreateUpdateContactInformation.as_view(),
+        name="contact-information",
+    ),
+    path("update-summary/", views.UpdateSummary.as_view(), name="update-summary"),
+    path("add-skill/<slug:skill>/", views.AddSkill.as_view(), name="add_skill"),
+    path(
+        "remove-skill/<slug:skill>/", views.RemoveSkill.as_view(), name="remove_skill"
+    ),
+    path(
+        "add-responsibility/",
+        views.AddResponsibility.as_view(),
+        name="add_responsibility",
+    ),
+    path(
+        "add-work_experience/",
+        views.AddWorkExperience.as_view(),
+        name="add_work_experience",
+    ),
 ]
